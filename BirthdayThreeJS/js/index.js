3f97aef8afdd918ed1593e3c6b1f05717006a60a
VALENTINE <valentine@VALENTINEs-MacBook-Pro-2.local>
2018-12-16 21:35:35 +0000

    update

--- BirthdayThreeJS/js/index.js
+++ BirthdayThreeJS/js/index.js
@@ -239,13 +239,13 @@ var render = function () {
   cakeWithFlame.rotation.y -= cakeRotSpeed;
 
 //random candle falme
-  time += clock.getDelta();
-
-  if(allFlameMaterials.length > 0) {
-    allFlameMaterials.map(function(flameMaterial, i) {
-      flameMaterial[0].uniforms.time.value = time + i;
-    })
-  }
+  // time += clock.getDelta();
+  //
+  // if(allFlameMaterials.length > 0) {
+  //   allFlameMaterials.map(function(flameMaterial, i) {
+  //     flameMaterial[0].uniforms.time.value = time + i;
+  //   })
+  // }
 
   // Balloon random movement
   var timer = balloonSpeed * Date.now();
